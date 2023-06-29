e0bcbbf4f413aeedf98f159021c9c167520bc284
Komal <kshekhawat2011@gmail.com>
2023-06-29 16:33:31 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyARR7HY8aYPj89zQKhqOqIcOoSymoGR2Do',
+  authDomain: 'chat-web-app-aa9bc.firebaseapp.com',
+  projectId: 'chat-web-app-aa9bc',
+  storageBucket: 'chat-web-app-aa9bc.appspot.com',
+  messagingSenderId: '410660836817',
+  appId: '1:410660836817:web:bbef2edfedccdc8be5fcf0',
+};
+
+const app = firebase.initializeApp(config);
