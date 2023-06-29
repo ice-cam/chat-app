bdcb56e78500ff896ad3921445ad53e6a5002f0f
Komal <kshekhawat2011@gmail.com>
2023-06-29 19:12:45 +0530

    added sign in page and sign in with facebook and google

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
 import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import Signin from './Pages/Signin';
+import Home from './Pages/Home';
 import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
 
