ebf5125434a5f688ff57a85bff79fd5e0b585684
Komal <kshekhawat2011@gmail.com>
2023-06-30 10:40:04 +0530

    started building dashboard , added signout button and dispaly user name

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import DashboardToggle from './dashboard/DashboardToggle';
+import DashboardToggle from './dashboard/DashBoardToggle';
 
 const Sidebar = () => {
   return (
