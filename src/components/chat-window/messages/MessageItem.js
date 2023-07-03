2ebb457b708712751bad8c0388c5396fa2f6b882
Komal <kshekhawat2011@gmail.com>
2023-07-03 18:09:46 +0530

    added button to grant or revoke admin access

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { memo } from 'react';
 import ProfileAvatar from '../../ProfileAvatar';
 //import { auth } from '../../../misc/firebase';
 import TimeAgo from 'timeago-react';
 import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 import PresenceDot from '../../PresenceDot';
+import { useCurrentRoom } from '../../../context/currentroomcontext';
+import { auth } from '../../../misc/firebase';
+import { Button } from 'rsuite';
 
-const MessageItem = ({ message }) => {
+const MessageItem = ({ message, handleAdmin }) => {
   const { author, createdAt, text } = message;
+  const isAdmin = useCurrentRoom(v => v.isAdmin);
+  const admins = useCurrentRoom(v => v.admins);
+
+  const isMsgAuthorAdmin = admins.includes(author.uid);
+  const isAuthor = auth.currentUser.uid === author.uid;
+  const canGrantAdmin = isAdmin && !isAuthor;
   return (
     <li className="padded mb-1">
       <div className="d-flex align-items-center font-bolder mb-1">
@@ -18,6 +27,13 @@ const MessageItem = ({ message }) => {
           size="xs"
         />
         <span className="ml-2">{author.name}</span>
+        {canGrantAdmin && (
+          <Button block onClick={() => handleAdmin(author.uid)} color="blue">
+            {isMsgAuthorAdmin
+              ? 'Remove admin permission'
+              : 'Give admin in this room'}
+          </Button>
+        )}
         <ProfileInfoBtnModal
           profile={author}
           appearence="link"
@@ -36,4 +52,4 @@ const MessageItem = ({ message }) => {
   );
 };
 
-export default MessageItem;
+export default memo(MessageItem);
