7abab9310c5400c6d4bfdb44d36fdbb8c443ba97
Komal <kshekhawat2011@gmail.com>
2023-07-04 10:55:05 +0530

    added an option to record and upolad audio file

--- src/components/chat-window/bottom/index.js
+++ src/components/chat-window/bottom/index.js
@@ -5,6 +5,7 @@ import { useProfile } from '../../../context/profile.context';
 import { useParams } from 'react-router-dom';
 import { database } from '../../../misc/firebase';
 import AttachmentBtnModal from './AttachmentBtnModal';
+import AudioMsgBtn from './AudioMsgBtn';
 
 function assembleMessage(profile, chatId) {
   return {
@@ -98,6 +99,7 @@ const Bottom = () => {
     <div>
       <InputGroup>
         <AttachmentBtnModal />
+        <AudioMsgBtn />
         <Input
           placeholder="Write a new message here..."
           value={input}
